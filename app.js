79761201d2978aa2a51a6deb49383b74772e7345
dahabu saidi <dahabusaidi@gmail.com>
2020-05-17 01:38:31 +0300

    connect to database

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// DB Connection
+mongoose.connect(process.env.MONGO_URI , { useNewUrlParser: true , useUnifiedTopology: true }).then(() => console.log("Database connected"))
+
+mongoose.connection.on("error", err =>
+{
+    console.log(`Database COnnection Error: ${ err.message }`)
+});
 
 // require routes
 
